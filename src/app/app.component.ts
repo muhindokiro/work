b756d6d6844cdfb44be26594d436da3e1d8eaf88
muhindokiro <blaize1143@gmail.com>
2019-05-31 11:40:16 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {Goal} from './goal'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+    goals = [
+       
+
+    ]
+}
